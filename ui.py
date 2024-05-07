57760452495b2bc1f5d22f0789f640b584e70c47
Aarsh Patel <aarsh18patel@gmail.com>
2024-05-07 14:22:01 +0530

    final commit

--- ui.py
+++ ui.py
@@ -11,21 +11,20 @@ class QuizInterface:
         self.window.config(pady=50, padx=50, bg=THEME_COLOR)
 
         self.score = 0
-        self.score_label = Label(text=f"Score: {self.score}", fg="white", bg=THEME_COLOR, pady=20,
+        self.score_label = Label(text=f"Score: {self.score}", fg="white", bg=THEME_COLOR,
                                  font=("Arial", 14, "normal"))
         self.score_label.grid(column=1, row=0)
 
-        self.canvas = Canvas(height=250, width=300)
-        self.canvas.create_text(125, 150, text="Hello World", font=("Arial", 20, "italic"), fill=THEME_COLOR)
-        self.canvas.grid(column=0, row=1, columnspan=2)
+        self.canvas = Canvas(height=400, width=300)
+        self.canvas.create_text(150, 200, text="Hello World", font=("Arial", 20, "italic"), fill=THEME_COLOR)
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
-        self.true_image = PhotoImage(file="images/true.png")
-        self.false_image = PhotoImage(file="images/false.png")
-
-        self.true_button = Button(image=self.true_image, highlightthickness=0)
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
         self.true_button.grid(column=0, row=2)
 
-        self.false_button = Button(image=self.false_image, highlightthickness=0)
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(column=1, row=2)
 
         self.window.mainloop()
