ed646bc31eeca5158933279b44b99717fd98af14
Aarsh Patel <aarsh18patel@gmail.com>
2024-05-07 14:46:42 +0530

    final commit

--- ui.py
+++ ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):  # here quiz_brain has datatype QuizBrain
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(pady=50, padx=50, bg=THEME_COLOR)
@@ -16,7 +18,8 @@ class QuizInterface:
         self.score_label.grid(column=1, row=0)
 
         self.canvas = Canvas(height=400, width=300)
-        self.canvas.create_text(150, 200, text="Hello World", font=("Arial", 20, "italic"), fill=THEME_COLOR)
+        self.question_text = self.canvas.create_text(150, 200, text="Hello World", font=("Arial", 18, "italic"),
+                                                     fill=THEME_COLOR, width=280)
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="images/true.png")
@@ -27,4 +30,10 @@ class QuizInterface:
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(column=1, row=2)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
